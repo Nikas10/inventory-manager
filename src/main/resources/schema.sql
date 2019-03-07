50fb757748f2332128790194d20cecc2c0f3af99
Nikas10 <nikas31415@gmail.com>
2019-03-07 19:22:29 +0300

    schema fix, readme addition

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -116,4 +116,4 @@ OIDS = FALSE
 TABLESPACE pg_default;
 
 
-INSERT INTO oauth_client_details VALUES ('web',NULL,'inventory','read,write,trust','password,authorization_code,refresh_token,implicit','','USER,ADMIN',NULL,NULL,NULL,NULL);
+INSERT INTO oauth_client_details VALUES ('web',NULL,'$2a$10$GHRBb68j3ZEhrehTuYXiLeShSWxSPq2KggDfeda5DJR0elQAWAzAy','read,write,trust','password,authorization_code,refresh_token,implicit','','USER,ADMIN',NULL,NULL,NULL,NULL);
