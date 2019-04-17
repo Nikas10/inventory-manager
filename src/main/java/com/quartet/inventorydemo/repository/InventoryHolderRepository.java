9d341b135ff4758c9e5d3844df291414756bc1cf
Gotus <stalnoe5555@mail.ru>
2019-04-18 00:18:29 +0300

    Method for searching holder with account was added.

--- src/main/java/com/quartet/inventorydemo/repository/InventoryHolderRepository.java
+++ src/main/java/com/quartet/inventorydemo/repository/InventoryHolderRepository.java
@@ -1,7 +1,9 @@
 package com.quartet.inventorydemo.repository;
 
+import com.quartet.inventorydemo.model.Account;
 import com.quartet.inventorydemo.model.InventoryHolder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Set;
@@ -9,9 +11,14 @@ import java.util.UUID;
 
 public interface InventoryHolderRepository extends JpaRepository<InventoryHolder, UUID> {
     List<InventoryHolder> findAll();
-
     Set<InventoryHolder> findByHolderIDIn(Iterable<UUID> ids);
     InventoryHolder findByHolderID(UUID holderID);
-
     List<InventoryHolder> findByName(String holderName);
+
+    @Query(
+        value = "SELECT * FROM  inventory_holder " +
+                "JOIN account_inventory_holder acc_inv " +
+                "ON acc_inv.holderid = inventory_holder.holderid",
+        nativeQuery = true)
+    List<Account> findHolderWithAccount();
 }
