cf9096555cb32ad034c05b446e3a95ee042bfbe0
Gotus <stalnoe5555@mail.ru>
2019-04-18 21:21:16 +0300

    Method for getting inventory position by id was added.

--- src/main/java/com/quartet/inventorydemo/rest/InventoryController.java
+++ src/main/java/com/quartet/inventorydemo/rest/InventoryController.java
@@ -1,13 +1,19 @@
 package com.quartet.inventorydemo.rest;
 
-import com.quartet.inventorydemo.model.InventoryItem;
-import com.quartet.inventorydemo.model.InventoryPositionContents;
-import com.quartet.inventorydemo.model.Requirement;
+import com.quartet.inventorydemo.model.*;
 import com.quartet.inventorydemo.service.*;
+import com.quartet.inventorydemo.util.Response;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/inventory")
@@ -37,5 +43,24 @@ public class InventoryController {
     @Qualifier("InventoryItemService")
     private InventoryItemService inventoryItemService;
 
+    @RequestMapping(value = "/{uuid}", method = RequestMethod.GET)
+    public ResponseEntity<?> getInventoryPosition(@PathVariable("uuid") String stringUuid) {
+        try {
+            InventoryPosition byPositionID = getPositionById(stringUuid);
+            return Response.createResponse(byPositionID);
+        } catch (IllegalArgumentException e) {
+            return Response.createErrorResponse(HttpStatus.BAD_REQUEST, "UUID is not correct");
+        } catch (NotFoundException e) {
+            return Response.createErrorResponse(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
 
+    private InventoryPosition getPositionById(String stringUuid) throws NotFoundException {
+        UUID uuid = UUID.fromString(stringUuid);
+        InventoryPosition byPositionID = inventoryPositionService.getByPositionID(uuid);
+        if (byPositionID == null) {
+            throw new NotFoundException("no role with uuid: " + stringUuid + " found");
+        }
+        return byPositionID;
+    }
 }
