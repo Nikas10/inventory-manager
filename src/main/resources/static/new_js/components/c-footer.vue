ae080206807834446c12c35e30b823cd66672b34
blinchiksmyasom <moljad@gmail.com>
2019-04-30 18:22:26 +0300

    Redo footer

--- src/main/resources/static/new_js/components/c-footer.vue
+++ src/main/resources/static/new_js/components/c-footer.vue
@@ -1,8 +1,29 @@
 <template>
-  <footer class="page-footer fixed-bottom font-small text-dark bg-light">
-    <div class="footer-copyright text-center py-3">
-      © 2019 Copyright:
-      <b-link to="about">Quartet Inc.</b-link>All rights reserved.
-    </div>
-  </footer>
+  <b-container>
+    <b-navbar class="navbar-fixed-bottom" fixed="bottom" type="light">
+      <b-container>
+        <p>
+          <center>
+            © 2019 Copyright:
+            <b-link to="about">Quartet Inc</b-link>. All rights reserved.
+          </center>
+        </p>
+      </b-container>
+    </b-navbar>
+  </b-container>
 </template>
+
+<style>
+.navbar-fixed-bottom {
+  position: absolute;
+}
+
+html {
+  position: relative;
+  min-height: 100%;
+}
+
+body {
+  margin-bottom: 70px;
+}
+</style>
