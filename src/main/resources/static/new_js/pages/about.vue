798e14f82caa5554898c6d32ac766ab5a58fd261
blinchiksmyasom <moljad@gmail.com>
2019-04-30 16:31:04 +0300

    Setup app page with vue.js

---
+++ src/main/resources/static/new_js/pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <c-header/>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  components: {
+    "c-header": httpVueLoader("new_js/components/c-header.vue")
+  },
+  data: function() {
+    return {};
+  }
+};
+</script>
