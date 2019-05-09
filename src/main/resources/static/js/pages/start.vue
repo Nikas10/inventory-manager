606e9c9a7213b0ef1c3011f46b467d052c31c5be
blinchiksmyasom <moljad@gmail.com>
2019-05-09 21:09:56 +0300

    Add table with all pages to start page

--- src/main/resources/static/js/pages/start.vue
+++ src/main/resources/static/js/pages/start.vue
@@ -1,5 +1,14 @@
 <template>
-  <c-default-page :storage="storage"></c-default-page>
+  <c-default-page :storage="storage">
+    <b-container>
+      <h1>All pages</h1>
+      <b-table small :items="pages" :fields="fields">
+        <template slot="title" slot-scope="data">
+          <b-link :to="data.item.link">{{data.value}}</b-link>
+        </template>
+      </b-table>
+    </b-container>
+  </c-default-page>
 </template>
 
 <script>
@@ -9,7 +18,80 @@ module.exports = {
   },
   props: ["storage"],
   data: function() {
-    return {};
+    return {
+      fields: {
+        title: {
+          label: "Page",
+          sortable: true
+        },
+        access: {
+          label: "Access",
+          sortable: true
+        }
+      },
+      pages: [
+        {
+          id: 0,
+          title: "About",
+          access: "User",
+          link: "/about"
+        },
+        {
+          id: 1,
+          title: "Holders",
+          access: "Staff",
+          link: "/holders/"
+        },
+        {
+          id: 2,
+          title: "Items",
+          access: "User",
+          link: "/items/"
+        },
+        {
+          id: 3,
+          title: "Profile",
+          access: "User",
+          link: "/profile"
+        },
+        {
+          id: 4,
+          title: "Requirements",
+          access: "Staff",
+          link: "/requirements/"
+        },
+        {
+          id: 5,
+          title: "Requisitions",
+          access: "User",
+          link: "/requisitions/"
+        },
+        {
+          id: 6,
+          title: "Roles",
+          access: "User",
+          link: "/roles/"
+        },
+        {
+          id: 7,
+          title: "Signin",
+          access: "User",
+          link: "/signin"
+        },
+        {
+          id: 8,
+          title: "Signup",
+          access: "User",
+          link: "/Signup"
+        },
+        {
+          id: 9,
+          title: "Users",
+          access: "Staff",
+          link: "/users/"
+        }
+      ]
+    };
   }
 };
 </script>
