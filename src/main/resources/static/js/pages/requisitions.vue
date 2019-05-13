62cde7d039c9d5fd6e85875e4334eda3c3e02c25
blinchiksmyasom <moljad@gmail.com>
2019-05-13 12:23:43 +0300

    Add list to requisitions page

--- src/main/resources/static/js/pages/requisitions.vue
+++ src/main/resources/static/js/pages/requisitions.vue
@@ -1,5 +1,10 @@
 <template>
-  <c-default-page :storage="storage"></c-default-page>
+  <c-default-page :storage="storage">
+    <b-container>
+      <h1>Requisitions</h1>
+      <b-table small :items="requisitions" :fields="fields"></b-table>
+    </b-container>
+  </c-default-page>
 </template>
 
 <script>
@@ -9,7 +14,61 @@ module.exports = {
   },
   props: ["storage"],
   data: function() {
-    return {};
+    return {
+      fields: {
+        id: {
+          label: "ID",
+          sortable: true
+        }
+      },
+      requisitions: []
+    };
+  },
+  methods: {
+    loadPage: function() {
+      if (!this.storage.user) {
+        // TODO
+        return;
+      }
+
+      if (this.storage.user.role == "user") {
+        this.loadOwnedRequisitions();
+      } else {
+        this.loadAllRequisitions();
+      }
+    },
+    loadAllRequisitions: function() {
+      const self = this;
+
+      this.$server
+        .get("/requisitions/")
+        .then(function(response) {
+          self.requisitions = response.data;
+        })
+        .catch(function(error) {
+          // TODO
+        });
+    },
+    loadOwnedRequisitions: function() {
+      const self = this;
+      this.$server
+        .get("/accounts/" + this.storage.user.login + "/requisitions/")
+        .then(function(response) {
+          self.requisitions = response.data;
+        })
+        .catch(function(error) {
+          // TODO
+        });
+    }
+  },
+  mounted: function() {
+    this.loadPage();
+  },
+  watch: {
+    "storage.user": function(a, b) {
+      // TODO не очень хорошо получается, что метод дёргается 2 раза, но пока так.
+      this.loadPage();
+    }
   }
 };
 </script>
