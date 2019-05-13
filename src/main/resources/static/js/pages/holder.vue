87a026eefad3428bb35cfb0f0738859fbf670828
blinchiksmyasom <moljad@gmail.com>
2019-05-13 13:38:34 +0300

    Add links for items on holder page

--- src/main/resources/static/js/pages/holder.vue
+++ src/main/resources/static/js/pages/holder.vue
@@ -36,8 +36,8 @@
       </b-table>
       <h2>Items</h2>
       <b-table small :items="items" :fields="itemsFields">
-        <template slot="login" slot-scope="data">
-          <b-link :to="'/users/' + data.item.login">{{data.value}}</b-link>
+        <template slot="name" slot-scope="data">
+          <b-link :to="'/positions/' + data.item.id">{{data.value}}</b-link>
         </template>
       </b-table>
     </b-container>
