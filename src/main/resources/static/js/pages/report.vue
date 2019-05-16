26b5359a985ba5aa84c449ccdc4d24582540483f
blinchiksmyasom <moljad@gmail.com>
2019-05-17 01:05:49 +0300

    Add report page

---
+++ src/main/resources/static/js/pages/report.vue
@@ -0,0 +1,32 @@
+<template>
+  <c-default-page :storage="storage">
+    <b-container well>
+      <h1>Report for user {{storage.user.login}}</h1>
+      <b-card>
+        <div id="rep"></div>
+      </b-card>
+    </b-container>
+  </c-default-page>
+</template>
+
+<script>
+module.exports = {
+  components: {
+    "c-default-page": httpVueLoader("js/components/c-default-page.vue")
+  },
+  props: ["storage"],
+  data: function() {
+    return {};
+  },
+  methods: {
+    loadReport: function() {
+      this.$server.get("report").then(function(response) {
+        document.getElementById("rep").innerHTML = response.data;
+      });
+    }
+  },
+  mounted: function() {
+    this.loadReport();
+  }
+};
+</script>
