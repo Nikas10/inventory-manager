3f8b1a70a7eefd0d90f401104eccb495f355f382
blinchiksmyasom <moljad@gmail.com>
2019-05-16 16:20:42 +0300

    Redo sign in page

--- src/main/resources/static/js/pages/signin.vue
+++ src/main/resources/static/js/pages/signin.vue
@@ -1,25 +1,25 @@
 <template>
   <c-default-page :storage="storage">
     <b-container>
-      <b-card title="Sign In">
-        <b-form @submit="onSubmit">
-          <b-form-group label="Login:">
-            <b-form-input id="login" v-model="form.login" required placeholder="Login"></b-form-input>
-          </b-form-group>
+      <b-form @submit="onSubmit" class="border border-light p-5">
+        <p class="h4 mb-4 text-center">Sign in</p>
 
-          <b-form-group label="Password:">
-            <b-form-input
-              id="password"
-              v-model="form.password"
-              type="password"
-              required
-              placeholder="Password"
-            ></b-form-input>
-          </b-form-group>
+        <b-form-group>
+          <b-form-input id="login" v-model="form.login" required placeholder="Login"></b-form-input>
+        </b-form-group>
 
-          <b-button type="submit" variant="primary">Submit</b-button>
-        </b-form>
-      </b-card>
+        <b-form-group>
+          <b-form-input
+            id="password"
+            v-model="form.password"
+            type="password"
+            required
+            placeholder="Password"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" block variant="primary">Sign In</b-button>
+      </b-form>
     </b-container>
   </c-default-page>
 </template>
@@ -42,7 +42,7 @@ module.exports = {
     onSubmit(evt) {
       const self = this;
 
-      this.$root.auth(this.form.login, this.form.password, function (){
+      this.$root.auth(this.form.login, this.form.password, function() {
         self.$router.go(-1);
       });
 
